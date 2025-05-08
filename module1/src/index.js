95cc12846b206ce9bee970289bf6961732c2afd2
nazneenlipi <lipinazneen65@gmail.com>
2025-05-09 04:29:27 +0600

    solve p2

--- module1/src/index.js
+++ module1/src/index.js
@@ -1,5 +1,3 @@
-// let dev: string = "hii"
-// console.log(dev)
 function formattingString(input, toUpper) {
     if (toUpper === false) {
         return input.toLowerCase();
@@ -8,6 +6,17 @@ function formattingString(input, toUpper) {
         return input.toUpperCase();
     }
 }
+// formattingString("Hello");         // "HELLO"
 console.log(formattingString("Hello")); // "HELLO"
 console.log(formattingString("Hello", true)); // "HELLO"
 console.log(formattingString("Hello", false)); // "hello"
+function filterByRating(items) {
+    return items.filter(function (item) { return item.rating > 4; });
+}
+var books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+filterByRating(books);
+console.log(filterByRating(books));
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -5,8 +5,21 @@ function formattingString(input:string, toUpper?:boolean ) : string {
         return input.toUpperCase()
     }
 }
+// formattingString("Hello");         // "HELLO"
 console.log(formattingString("Hello"));         // "HELLO"
 console.log(formattingString("Hello", true));   // "HELLO"
 console.log(formattingString("Hello", false));  // "hello"
 
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+   return items.filter((item) => item.rating > 4)
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+filterByRating(books); 
+console.log(filterByRating(books));
 
