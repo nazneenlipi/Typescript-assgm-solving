67c4190693bfb51afccd5bcbbbfa99ddafe04535
nazneenlipi <lipinazneen65@gmail.com>
2025-05-09 11:56:02 +0600

    solve 4

--- module1/src/index.js
+++ module1/src/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function formattingString(input, toUpper) {
     if (toUpper === false) {
         return input.toLowerCase();
@@ -16,7 +31,40 @@ function filterByRating(items) {
 var books = [
     { title: "Book A", rating: 4.5 },
     { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
+    { title: "Book C", rating: 5.0 },
 ];
 filterByRating(books);
 console.log(filterByRating(books));
+// type GenericArray<T> = Array<T>
+// function concatenateArrays<T>(...arrays: T[][]): T[]{
+//   // return GenericArray <string>=["a", "b", "c", "d"]
+// }
+// concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+// concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+// Create a Vehicle class with private make and year properties and a getInfo() method.
+// Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+var Vehicle = /** @class */ (function () {
+    function Vehicle(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    Vehicle.prototype.getInfo = function () {
+        console.log("This car is making ".concat(this.make, " in ").concat(this.year));
+    };
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(make, year, model) {
+        var _this = _super.call(this, make, year) || this;
+        _this.model = model;
+        return _this;
+    }
+    Car.prototype.getModel = function () {
+        console.log("This is the ".concat(this.model, " model"));
+    };
+    return Car;
+}(Vehicle));
+var myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo(); // Output: "Make: Toyota, Year: 2020"
+myCar.getModel(); // Output: "Model: Corolla"
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -1,25 +1,63 @@
-function formattingString(input:string, toUpper?:boolean ) : string {
-    if(toUpper === false){
-        return input.toLowerCase()
-    }else{
-        return input.toUpperCase()
-    }
+function formattingString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
 }
 // formattingString("Hello");         // "HELLO"
-console.log(formattingString("Hello"));         // "HELLO"
-console.log(formattingString("Hello", true));   // "HELLO"
-console.log(formattingString("Hello", false));  // "hello"
+console.log(formattingString("Hello")); // "HELLO"
+console.log(formattingString("Hello", true)); // "HELLO"
+console.log(formattingString("Hello", false)); // "hello"
 
-
-function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
-   return items.filter((item) => item.rating > 4)
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating > 4);
 }
 const books = [
   { title: "Book A", rating: 4.5 },
   { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 }
+  { title: "Book C", rating: 5.0 },
 ];
 
-filterByRating(books); 
+filterByRating(books);
 console.log(filterByRating(books));
 
+// type GenericArray<T> = Array<T>
+
+// function concatenateArrays<T>(...arrays: T[][]): T[]{
+//   // return GenericArray <string>=["a", "b", "c", "d"]
+// }
+
+// concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+// concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+
+
+class Vehicle {
+   private make: string;
+  private year: number;
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+ getInfo() {
+  console.log(`This car was made by ${this.make} in ${this.year}`);
+}
+}
+
+class Car extends Vehicle {
+    private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(`This is the ${this.model} model`);
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo(); // Output: "Make: Toyota, Year: 2020"
+myCar.getModel(); // Output: "Model: Corolla"
